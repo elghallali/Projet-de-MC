d8446658afc1a80510f96107324abace0754dbd8
Yassine EL GHALLALI <y.elghallali@gmail.com>
2023-01-30 14:11:02 +0100

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -10,27 +10,29 @@ import re
 # Create Analyse class
 class Analyse:
 
-    def __init__(self,location, tab):
-        self.location = location
-        self.excel_files = glob.glob(location+'\\*.xlsx')
-        self.indices_list = tab
+    # Constructor Method
+    def __init__(self,location):
+        self.__location = location
+        self.__excel_files = glob.glob(self.__location+'\\*.xlsx')
+        self.__indices_list = ['MASI', 'MSI20']
+
 
     # Creation the necessary folders
-    def createNewDirectories(self):
+    def __createNewDirectories(self):
         new_folders = ['Data','images']
-        os.chdir(self.location)
+        os.chdir(self.__location)
         for i in new_folders:
-            if not os.path.exists(self.location+'\\'+i):
+            if not os.path.exists(self.__location+'\\'+i):
                 os.makedirs(i)
             if i == 'images':
-                path = location + '\\'+ i
+                path = self.__location + '\\'+ i
                 os.chdir(path)
-                for j in self.indices_list:
+                for j in self.__indices_list:
                     if not os.path.exists(path+'\\'+j):
                         os.makedirs(j)
 
     # Ratio Method
-    def ratio(self, dataFrame, indice):
+    def __ratio(self, dataFrame, indice):
         df = pd.DataFrame()
         dataFrame_list = list(dataFrame.columns)
         df['Date'] = pd.to_datetime(dataFrame['Date'], format='%d/%m/%Y')
@@ -42,10 +44,10 @@ class Analyse:
         for i in new_data_index:
             new_row.append(new_data[i])
         df.loc[255] = new_row
-        df.to_excel(f'{self.location}\\Data\\Ratio{indice}.xlsx', index = False)
+        df.to_excel(f'{self.__location}\\Data\\Ratio{indice}.xlsx', index = False)
 
     # Correlation Method for creating Excel table
-    def correlation(self, dataFrame, indice):
+    def __correlation(self, dataFrame, indice):
         df = pd.DataFrame()
         dataFrame_list = list(dataFrame.columns)
         df[indice] = dataFrame[indice]
@@ -53,52 +55,43 @@ class Analyse:
             df[dataFrame_list[i]] = dataFrame[dataFrame_list[i]]
         df_corr = df.corr()
         sns.heatmap(df_corr, cmap='RdBu', center=0, vmin=-1, vmax=1)
-        plt.savefig(f'{self.location}\\images\\Correlation{indice}.png')
+        plt.savefig(f'{self.__location}\\images\\Correlation{indice}.png')
         plt.clf()
-        df_corr.to_excel(f'{self.location}\\Data\\Correlation{indice}.xlsx')
+        df_corr.to_excel(f'{self.__location}\\Data\\Correlation{indice}.xlsx')
 
     # Correlation Images Method
-    def correlationImage(self, dataFrame, indice):
+    def __correlationImage(self, dataFrame, indice):
         dataFrame_list = list(dataFrame.columns)
         for i in range(3, len(dataFrame_list)):
             sns.set()
             sns.regplot(x=dataFrame[indice], y=dataFrame[dataFrame_list[i]], data=dataFrame )
-            plt.savefig(f'{self.location}\\images\\{indice}\\Corr{indice}{dataFrame_list[i]}.png')
+            plt.savefig(f'{self.__location}\\images\\{indice}\\Corr{indice}{dataFrame_list[i]}.png')
             plt.clf()
 
     # Create Excel files and Images
     def create_files(self):
-        for file in self.excel_files:
+        self.__createNewDirectories()
+        for file in self.__excel_files:
             df = pd.read_excel(file, date_parser='%d/%m/%Y')
-            for indice in self.indices_list:
-                self.ratio(df, indice)
-                self.correlationImage(df, indice)
-                self.correlation(df, indice)
+            for indice in self.__indices_list:
+                self.__ratio(df, indice)
+                self.__correlationImage(df, indice)
+                self.__correlation(df, indice)
 
 # Create Location path and a list of indexes
 def createLocation():
     locationInput = input('Enter files location : ')
     location = locationInput.replace('\\', '\\\\')
-    num = int(input('Enter the number of indexes : '))
-    tab = []
-    for i in range(num):
-        indice = input('Enter Index : ')
-        tab.append(indice.upper())
-    return location,tab
-
-# Create default dataset
-def createDataSet(location):
-    test = input("Entrer Oui/Yes : si vous necessites une dataset par default et Non/No : sinon : ")
-    test = test.lower()
-    if test in ['yes', 'oui', 'si']:
-        df = pd.read_excel('./MC_DATA_2022.xlsx', date_parser='%d/%m/%Y')
-        df.to_excel(f'{location}\\DATA.xlsx', index = False)
+    df = pd.read_excel('./MC_DATA_2022.xlsx', date_parser='%d/%m/%Y')
+    df.to_excel(f'{location}\\DATA.xlsx', index = False)
+    return location
 
 
-location, tab = createLocation()
-createDataSet(location)
-analyse1 = Analyse(location, tab)
-analyse1.createNewDirectories()
+
+
+
+location= createLocation()
+analyse1 = Analyse(location)
 analyse1.create_files()
 
 
